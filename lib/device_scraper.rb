2438d6b3b69c10dc5c5572f0526d3dab1dce7efa
Code Ass <aycabta@gmail.com>
2017-03-10 03:13:13 +0900

    Add lib/device_scraper.rb

---
+++ lib/device_scraper.rb
@@ -0,0 +1,78 @@
+require 'mechanize'
+require 'uri'
+
+class DeviceScraper
+  def initialize(device_model)
+    @device_model = device_model
+  end
+
+  def run
+    agent = Mechanize.new
+    agent.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36'
+    puts 'log in try...'
+    page = agent.get(oauth_url)
+    form = page.form_with(name: 'signIn', id: 'ap_signin_form') { |f|
+      f.email = Rails.application.secrets.amazon_email
+      f.password = Rails.application.secrets.amazon_password
+      f.checkbox_with(name: 'rememberMe').check
+    }
+    loggedin = form.submit
+    puts 'logged in'
+
+    puts 'press okey'
+    form = loggedin.form_with(name: 'consent-form')
+    okey = form.button_with(name: 'consentApproved')
+    choose = form.click_button(okey)
+    puts 'done'
+
+    slot_set = recursive_choose(choose)
+    puts 'end'
+    slot_set
+  end
+
+  def recursive_choose(choose)
+    title = choose.search('.header').search('.title').search('span').attr('title').value
+    slot_id = choose.search('//input[@name="slotId"]').attr('value').value
+    items = choose.search('.a-row .products').first.search('.product')
+    asin_list = items.map { |i| i.attributes['id'].value }
+    puts title
+    puts slot_id
+    puts asin_list
+
+    form = choose.form_with(action: '/slot_action')
+    form.radiobutton_with(name: 'asin', value: asin_list.first).check
+    next_button = form.button_with(class: 'a-button-input')
+    choose = form.click_button(next_button)
+    data = {
+      slot_id: slot_id,
+      asin: asin_list,
+      title: title
+    }
+    if choose.uri.to_s != 'https://drs-web.amazon.com/setup_overview'
+      recursive_choose(choose).push(data)
+    else
+      [data]
+    end
+  end
+
+  def oauth_url
+    redirect_uri = "http://localhost:55582/"
+    base = 'https://www.amazon.com/ap/oa?'
+    client_id = 'amzn1.application-oa2-client.b5c87429af104636bd7ae83df68383e1'
+    serial = generate_serial
+    params = {
+      client_id: client_id,
+      scope: 'dash:replenish',
+      response_type: 'code',
+      redirect_uri: URI.encode_www_form_component(redirect_uri),
+      scope_data: %Q`{"dash:replenish":{"device_model":"#{@device_model}","serial":"#{serial}"#{ ',"is_test_device":true' if @is_test }}}`.gsub('"', '%22')
+    }
+    "#{base}#{params.map{ |k, v| "#{k}=#{v}" }.join(?&)}"
+  end
+
+  def generate_serial
+    orig = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map { |i| i.to_a }.flatten
+    random_suffix = (0...16).map { orig[rand(orig.size)] }.join
+    "#{@device_model}_#{Time.now.to_i}_#{random_suffix}"
+  end
+end
