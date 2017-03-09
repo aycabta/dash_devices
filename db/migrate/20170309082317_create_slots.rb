1bcff71c6149fbc397f4005f27d3fa1697f80538
Code Ass <aycabta@gmail.com>
2017-03-09 17:24:33 +0900

    Add Slot model

---
+++ db/migrate/20170309082317_create_slots.rb
@@ -0,0 +1,11 @@
+class CreateSlots < ActiveRecord::Migration[5.0]
+  def change
+    create_table :slots do |t|
+      t.integer :device_id
+      t.string :slot_id
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
