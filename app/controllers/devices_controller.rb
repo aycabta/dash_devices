72c88c1e3ef8d05fcfab761427a728b40f944ffa
Code Ass <aycabta@gmail.com>
2017-03-12 02:54:19 +0900

    Add GET '/devices/:device_model' on 'devices#show'

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -2,4 +2,8 @@ class DevicesController < ApplicationController
   def index
     @devices = Device.all
   end
+
+  def show
+    @device = Device.find_by(model: params[:device_model])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/', to: 'devices#index'
+  get '/devices/:device_model', to: 'devices#show', constraints: { device_model: /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/ }
+  get '/devices/:device_model', to: redirect(status: 303) { |params, request| "/devices/#{params[:device_model].downcase}" }, constraints: { device_model: /(?!.*[A-Z])[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/ }
 end
