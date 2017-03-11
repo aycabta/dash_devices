3e340142a132909ca3bb823c96a0c7b698a43cf9
Code Ass <aycabta@gmail.com>
2017-03-11 21:29:17 +0900

    Use Haml for default template engine

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,9 @@ module DashDevices
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += %W(#{config.root}/lib)
+    config.generators do |g|
+      g.template_engine :haml
+      g.test_framework :test_unit
+    end
   end
 end
