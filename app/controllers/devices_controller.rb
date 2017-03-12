e0d38331640574cb4100f7e610dfb91f7ae1acc0
Code Ass <aycabta@gmail.com>
2017-03-12 21:10:08 +0900

    Add create form at index

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,9 +1,15 @@
 class DevicesController < ApplicationController
   def index
     @devices = Device.all
+    @new_device = Device.new
   end
 
   def show
     @device = Device.find_by(model: params[:device_model])
+  end
+
+  def create
+    @device = Device.find_or_create_by(name: params[:device][:name], model: params[:device][:model])
+    redirect_to action: 'show', device_model: @device.model
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/', to: 'devices#index'
   get '/devices/:device_model', to: 'devices#show', constraints: { device_model: /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/ }
   get '/devices/:device_model', to: redirect(status: 303) { |params, request| "/devices/#{params[:device_model].downcase}" }, constraints: { device_model: /(?=.*[A-Z])\h{8}-\h{4}-\h{4}-\h{4}-\h{12}/ }
+  post '/devices/create', to: 'devices#create'
 end
