9dede783d0fd6c1232b08d8b4f35201bd5e16380
Code Ass <aycabta@gmail.com>
2017-03-17 00:01:09 +0900

    Add "null: false" to some columns

--- db/migrate/20170309081422_create_devices.rb
+++ db/migrate/20170309081422_create_devices.rb
@@ -2,8 +2,8 @@ class CreateDevices < ActiveRecord::Migration[5.0]
   def change
     create_table :devices do |t|
       t.integer :user_id
-      t.string :model
-      t.string :name
+      t.string :model, null: false
+      t.string :name, null: false
       t.boolean :scraped, default: false, null: false
       t.index :model, :unique => true
 
--- db/migrate/20170309082317_create_slots.rb
+++ db/migrate/20170309082317_create_slots.rb
@@ -2,8 +2,8 @@ class CreateSlots < ActiveRecord::Migration[5.0]
   def change
     create_table :slots do |t|
       t.integer :device_id
-      t.string :drs_slot_id
-      t.string :name
+      t.string :drs_slot_id, null: false
+      t.string :name, null: false
       t.index :drs_slot_id, :unique => true
 
       t.timestamps
