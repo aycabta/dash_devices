b2369881da5b7d703b37dbaeb19b5c24f2bb00d9
Code Ass <aycabta@gmail.com>
2017-03-18 02:45:50 +0900

    Add link_to user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'devices#index'
   get '/devices/:device_model', to: 'devices#show'
+  get '/users/:id', to: 'users#show', as: :users_show
   post '/devices/create', to: 'devices#create'
   get '/auth/:provider/callback', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
