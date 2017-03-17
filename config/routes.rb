c4e0b60e2712a94374f7a228f8efff023c419a75
Code Ass <aycabta@gmail.com>
2017-03-18 01:51:53 +0900

    Remove constraints because device_model is user defined string

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'devices#index'
-  get '/devices/:device_model', to: 'devices#show', constraints: { device_model: /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/ }
-  get '/devices/:device_model', to: redirect(status: 303) { |params, request| "/devices/#{params[:device_model].downcase}" }, constraints: { device_model: /(?=.*[A-Z])\h{8}-\h{4}-\h{4}-\h{4}-\h{12}/ }
+  get '/devices/:device_model', to: 'devices#show'
   post '/devices/create', to: 'devices#create'
   get '/auth/:provider/callback', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
