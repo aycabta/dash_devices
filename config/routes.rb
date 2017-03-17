92e322e390c349956ce9f314c1521ad329fa575f
Code Ass <aycabta@gmail.com>
2017-03-18 02:50:32 +0900

    Add :device_show named helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'devices#index'
-  get '/devices/:device_model', to: 'devices#show'
+  get '/devices/:device_model', to: 'devices#show', as: :device_show
   get '/users/:id', to: 'users#show', as: :users_show
   post '/devices/create', to: 'devices#create'
   get '/auth/:provider/callback', to: 'sessions#create'
